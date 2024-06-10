f28a4925aace1eb0790f071f901a16db6d81f46d
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-06-09 21:25:17 -0300

    Switch agregados al tramite

--- app/public/form.js
+++ app/public/form.js
@@ -1,41 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-const animalYes = document.querySelector('#animalSi');
-const animalNo = document.querySelector('#animalNo');
+const animal = document.querySelector('#animal');
 function toggleAnimalButtons()
 {
-    console.log(animalYes);
-    if(animalYes.checked)
+    console.log(animal);
+    if(animal.checked)
         {
-            const vaccinedYesBtn = document.querySelector('#vacunadoSi');
-            vaccinedYesBtn.disabled = false;
-            const vaccinedNoBtn = document.querySelector('#vacunadoNo');
-            vaccinedNoBtn.disabled = false;
+            const vaccinedBtn = document.querySelector('#vacunado');
+            vaccinedBtn.disabled = false;            
 
-            const castratedYesBtn = document.querySelector('#castradoSi');
-            castratedYesBtn.disabled = false;
-            const castratedNoBtn = document.querySelector('#castradoNo');
-            castratedNoBtn.disabled = false;
+            const castratedBtn = document.querySelector('#castrado');
+            castratedBtn.disabled = false;            
         }
     else
     {
-        const vaccinedYesBtn = document.querySelector('#vacunadoSi');
-        vaccinedYesBtn.disabled = true;
-        vaccinedYesBtn.checked = false;
-        const vaccinedNoBtn = document.querySelector('#vacunadoNo');
-        vaccinedNoBtn.disabled = true;
-        vaccinedNoBtn.checked = false;
+        const vaccinedBtn = document.querySelector('#vacunado');
+        vaccinedBtn.disabled = true;
+        vaccinedBtn.checked = false;       
 
-        const castratedYesBtn = document.querySelector('#castradoSi');
-        castratedYesBtn.disabled = true;
-        castratedYesBtn.checked = false;
-        const castratedNoBtn = document.querySelector('#castradoNo');
-        castratedNoBtn.disabled = true;
-        castratedNoBtn.checked = false;
+        const castratedBtn = document.querySelector('#castrado');
+        castratedBtn.disabled = true;
+        castratedBtn.checked = false;        
     }
 }
 
-animalYes.addEventListener('click', () => {toggleAnimalButtons()});
-animalNo.addEventListener('click', () => {toggleAnimalButtons()});
+animal.addEventListener('click', () => {toggleAnimalButtons()});
 
 function toggleChildAge()
 {
